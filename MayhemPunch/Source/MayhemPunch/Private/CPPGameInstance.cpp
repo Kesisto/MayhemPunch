5f6517e2e0170c41e65db64dd4187a88758e4fbf
VilleLP <ville_l-p@suomi24.fi>
2015-08-21 13:17:48 +0300

    jåå

--- MayhemPunch/Source/MayhemPunch/Private/CPPGameInstance.cpp
+++ MayhemPunch/Source/MayhemPunch/Private/CPPGameInstance.cpp
@@ -16,12 +16,17 @@ void UCPPGameInstance::HideMouse(bool Hide, APlayerController* PlayerController)
 	FViewport* Viewport = player->ViewportClient->Viewport;
 	
 	if (Hide) {
+		PlayerController->bShowMouseCursor = false;
 		MousePositionX = Viewport->GetMouseX();
 		MousePositionY = Viewport->GetMouseY();
 		Viewport->SetMouse(0, 0);
 		Change = true;
 	}
 	else if (Change) {
+		PlayerController->bShowMouseCursor = true;
+		PlayerController->bEnableclickEvents = true;
+		PlayerController->bEnableTouchEvents = true;
+		PlayerController->bEnableMouseOverEvents = true;
 		Viewport->SetMouse(MousePositionX, MousePositionY);
 		Change = false;
 	}
